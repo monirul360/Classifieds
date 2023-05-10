4471d842efd3964c94d441bb43a4b73281e015da
Monirul360e <monirulislam65361@gmail.comm>
2023-05-10 18:06:31 +0600

    footer design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './Component/Share/Footer/Footer';
 import Nav from './Component/Share/Nav/Nav';
 import Home from './Page/Home/Home';
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Nav></Nav>
       <Home></Home>
+      <Footer></Footer>
     </>
   );
 }
